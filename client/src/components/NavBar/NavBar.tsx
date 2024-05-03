0ef1501a5ea3ce5aed7459fcb7f5d2b4bb84f07b
Kirk Santos <kirkalyn13@gmail.com>
2024-05-03 23:41:01 +0800

    feat: NavBar component

---
+++ client/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { BiMenu, BiLogOut } from 'react-icons/bi'
+
+const LOGO_SRC = "../../public/favicon.ico"
+const Header : React.FC = () => {
+  const [ showMenu, setShowMenu ] = useState(false)
+  
+  return (
+    <nav className="w-screen p-4 z-10">
+        <div className="container mx-auto flex flex-wrap justify-between items-center">
+            <div className="flex items-center">
+                <img
+                    className="me-4"
+                    src={LOGO_SRC}
+                    width={50}
+                    height={50}
+                    alt="logo"/>
+            </div>
+
+            <div className="md:hidden mt-4">
+                <button 
+                    className="text-black"
+                    onClick={() => setShowMenu(!showMenu)}>
+                    <BiMenu className="text-4xl"/>
+                </button>
+            </div>
+
+            <ul className="hidden md:flex space-x-4 align-center items-center">
+                <li><a href="#" className="text-black hover:text-amber-500">Books</a></li>
+                <li><a href="#authors" className="text-black hover:text-amber-500">Authors</a></li>
+                <li><a href="#publishers" className="text-black hover:text-amber-500">Publishers</a></li>
+            </ul>
+        </div>
+
+        { showMenu ?
+                (<ul className="space-y-2 text-center">
+                    <li><a href="#" className="text-black border-t-1 hover:text-amber-500">Home</a></li>
+                    <li><a href="#authors" className="text-black hover:text-amber-500">Voltage</a></li>
+                    <li><a href="#publishers" className="text-black hover:text-amber-500">Current</a></li>
+                </ul>)
+            : null}
+
+    </nav>
+  )
+}
+
+export default Header
