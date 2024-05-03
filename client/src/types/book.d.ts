2fd45ff84f9bfc633a2ca8edfcb9b399d8353b70
Kirk Santos <kirkalyn13@gmail.com>
2024-05-03 23:41:26 +0800

    feat: book types

---
+++ client/src/types/book.d.ts
@@ -0,0 +1,12 @@
+type Book = {
+    id: number
+    title: string
+    isbn13: string
+    isbn10: string
+    listPrice: number
+    publicationYear: number
+    imageURL: string
+    edition: string
+    publisherID: number
+    authors: Author[]
+}
