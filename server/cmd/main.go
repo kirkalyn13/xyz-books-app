133b39e24326d11e842c9ce139ff28beb6e66268
Kirk Santos <kirkalyn13@gmail.com>
2024-05-04 20:41:58 +0800

    feat: cors

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kirkalyn13/xyz-books-app/cmd/routes"
 	"github.com/kirkalyn13/xyz-books-app/pkg/db"
@@ -14,6 +16,14 @@ func main() {
 	db.LoadDatabase()
 
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"http://localhost:5173"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:  []string{"Origin"},
+		ExposeHeaders: []string{"Content-Length"},
+		MaxAge:        12 * time.Hour,
+	}))
+
 	routes.RegisterRoutes(r)
 
 	r.Run()
