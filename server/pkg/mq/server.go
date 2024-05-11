4d9dc1faccd2aede3fb9dd1fc5db2f17c3f5196e
Kirk Santos <kirkalyn13@gmail.com>
2024-05-12 00:21:38 +0800

    fix: rabbitmq url

--- server/pkg/mq/server.go
+++ server/pkg/mq/server.go
@@ -3,7 +3,7 @@ package mq
 const (
 	// Server is the URL for Rabbit MQ instance.
 	// Note that this is only for demo purposes and should not be included in the code.
-	Server = "amqp://guest:guest@localhost:5672/"
+	Server = "amqp://guest:guest@rabbitmq"
 
 	// BookQueue is the queue name for the books API
 	BookQueue = "xyz-books"
